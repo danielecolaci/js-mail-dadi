a445bcfcaa338354604f086f73ccf2f3b6cbb8b3
danielecolaci <danielecolaci@outlook.com>
2024-02-22 16:48:48 +0100

    Add Condition and Result on Console

--- Dadi/assets/js/main.js
+++ Dadi/assets/js/main.js
@@ -3,3 +3,13 @@ let numberComputer = Math.floor(Math.random() * 6) + 1;
 
 console.log('Il tuo punteggio è ' + numberHuman)
 console.log('Il punteggio del Computer è ' + numberComputer)
+
+if (numberHuman > numberComputer) {
+    console.log('Hai vinto!');
+}
+else if (numberHuman == numberComputer) {
+    console.log('Pareggio!');
+}
+else {
+    console.log('Hai perso!');
+}
