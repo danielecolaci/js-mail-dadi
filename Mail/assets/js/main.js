f4c68750c93e5d2212cebecabe79df360ad59165
danielecolaci <danielecolaci@outlook.com>
2024-02-22 16:17:07 +0100

    Add Prompt and Alert on Mail Verification. It works

--- Mail/assets/js/main.js
+++ Mail/assets/js/main.js
@@ -1,1 +1,21 @@
-console.log('ok');
+/* const mailInserita = document.getElementById('mail').value;
+console.log(mailInserita); */
+
+let mailAutorizzate = ['daniele@gmail.com', 'daniele@live.com', 'daniele@outlook.com', 'daniele@hotmail.com', 'daniele@libero.com']
+let mailInsert = prompt('Inserisci la tua mail')
+let mail = false
+
+
+for (let index = 0; index < mailAutorizzate.length; index++) {
+    const list = mailAutorizzate[index];
+    if (list == mailInsert) {
+        mail = true;
+    }
+}
+
+if (mail == true) {
+    alert('Mail Autorizzata. Welcome!')
+}
+else {
+    alert('Mail non Autorizzata. Riprova')
+}
